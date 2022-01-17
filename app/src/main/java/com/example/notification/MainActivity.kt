1809bd17bc006619ad299a1b654a3ec7e1837931
dzaky1201 <dzakyhdr@gmail.com>
2022-01-17 21:05:43 +0700

    create-example-notif

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.core.app.NotificationCompat
 import com.example.notification.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
+    private var notificationManager: NotificationManager? = null
+    private val channel_id = "channel_1"
     private lateinit var binding: ActivityMainBinding
+    private lateinit var countDownTimer: CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +23,51 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        // register channel kedalam sistem
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(channel_id, "countdown", "Notif when countdown end")
+
+        binding.btnStart.setOnClickListener {
+            countDownTimer.start()
+        }
+
+        countDownTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                binding.timer.text = getString(R.string.time_reamining, p0 / 1000)
+            }
+
+            override fun onFinish() {
+               displayNotification()
+            }
+        }
+
+
+
+    }
+
+    private fun displayNotification() {
+        val notificationId = 45
+        val notification = NotificationCompat.Builder(this@MainActivity, channel_id)
+            .setContentTitle("Countdown Timer")
+            .setContentText("Your timer end")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+        notificationManager?.notify(notificationId, notification)
+    }
+
+    private fun createNotificationChannel(id: String, name: String, channelDescription: String) {
+        // validasi notif akan dibuat jika version sdk 26+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // set interupsi notifikasi
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            // membuat channel
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = channelDescription
+            }
+            // mendaftarkan channel ke dalam sistem notification manager
+            notificationManager?.createNotificationChannel(channel)
+        }
     }
 }
